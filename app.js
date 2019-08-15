4294c10a3e1a938b914bbf595814f1a3c5ee320b
jofer86 <jofer86@gmail.com>
2019-08-15 17:08:42 -0500

    Fixed eslint Issues

--- app.js
+++ app.js
@@ -1,32 +1,3 @@
-const GameRunner = (() => {
-  const gameBoard = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  const playersList = Players();
-  let index = 0;
-  const currentPlayer = () => playersList.getPlayers()[index];
-  const changePlayer = () => { index = index === 1 ? 0 : 1; };
-  const createPlayers = () => {
-    const player1 = playersList.add(PlayerFactory(UIController().playerOneInput.value, 'X'));
-    const player2 = playersList.add(PlayerFactory(UIController().playerTwoInput.value, 'O'));
-    UIController().board.classList.remove('hide');
-    const html = `
-      <div class="player player-0"><span>Player 1: </span><span>${player1.getPlayerName()}</span></div>
-      <div class="player player-1"><span>Player 2: </span><span>${player2.getPlayerName()}</span></div>
-    `;
-    UIController().inputDetails.innerHTML = html;
-    UIController()[`player-${index}`].classList.add('scale');
-    UIController().statusBanner.classList.remove('hide');
-    UIController().inputDetails.classList.add('ongoing');
-    UIController().statusText.innerText = `${player1.getPlayerName()}'s turn`;
-  };
-  const reload = () => location.reload();
-  const checkWin = () => CheckWin();
-
-  return {
-    // eslint-disable-next-line comma-dangle
-    playersList, createPlayers, currentPlayer, changePlayer, gameBoard, checkWin, reload
-  };
-})();
-
 const GameBoard = () => {
   const board = GameRunner.gameBoard;
   const position = {
@@ -56,7 +27,7 @@ const GameBoard = () => {
 const Players = () => {
   const players = [];
   const getPlayers = () => { return players; };
-  const add = function (player) {
+  const add = (player) => {
     players.push(player);
     return player;
   };
@@ -94,73 +65,90 @@ const CheckWin = () => {
     UIController().reload.classList.add('color-2');
     UIController().reload.addEventListener('click', GameRunner.reload);
     return true;
-  } else if ((horTop === o) || (horMid === o) || (horDow === o) || (verLeft === o)
+  } if ((horTop === o) || (horMid === o) || (horDow === o) || (verLeft === o)
   || (verMid === o) || (verRight === o) || (diagOne === o) || (diagTwo === o)) {
     UIController().statusText.innerText = 'player 2 is the winner!!';
     UIController().statusText.classList.add('color-1');   
-    UIController().cells.forEach((cell)=>{cell.removeAttribute('onclick')});
+    UIController().cells.forEach((cell) => { cell.removeAttribute('onclick')});
     UIController().reload.classList.add('show');
-    UIController().reload.addEventListener('click', GameRunner.reload)
+    UIController().reload.addEventListener('click', GameRunner.reload);
     return true;
-  } else {
-    GameRunner.changePlayer()
-    UIController().statusText.innerText = `${GameRunner.currentPlayer().name}'s turn`;
-
-    return false;
   }
 
-}
+  GameRunner.changePlayer();
+  UIController().statusText.innerText = `${GameRunner.currentPlayer().name}'s turn`;
+
+  return false;
+};
 
 const UIController = () => {
-  return {    
-      playerOneInput: document.querySelector('.nameInput-1'),
-      playerTwoInput: document.querySelector('.nameInput-2'),
-      board: document.querySelector('.board'),
-      inputDetails: document.querySelector('.player-details'),
-      cells: document.querySelectorAll('.cell'),
-      statusBanner: document.querySelector('.events'),
-      statusText: document.querySelector('.status'),
-      "player-0": document.querySelector('.player-0'),
-      "player-1": document.querySelector('.player-1'),
-      reload: document.querySelector('.reload')
-  }
-}
+  return {
+    playerOneInput: document.querySelector('.nameInput-1'),
+    playerTwoInput: document.querySelector('.nameInput-2'),
+    board: document.querySelector('.board'),
+    inputDetails: document.querySelector('.player-details'),
+    cells: document.querySelectorAll('.cell'),
+    statusBanner: document.querySelector('.events'),
+    statusText: document.querySelector('.status'),
+    'player-0': document.querySelector('.player-0'),
+    'player-1': document.querySelector('.player-1'),
+    reload: document.querySelector('.reload')
+  };
+};
 
 const clickChange = (clickID,i) => {
-  let change = document.getElementById(clickID);
+  const change = document.getElementById(clickID);
   let mark =  GameRunner.currentPlayer().getPlayermark();
 
-  if(mark === "X"){
+  if (mark === 'X') {
+    change.classList.add ('color-red');
+    
+  } else if (mark === 'O') {    
+    change.classList.add('color-blue');
+  } else {
     change.classList.add('color-red');
-    
-  }else if(mark === "O"){    
-    change.classList.add('color-blue')
-    
-  }else {
-    change.classList.add('color-red')    
-  }  
-  change.innerHTML= mark
-  change.removeAttribute('onclick')
+  }
+  change.innerHTML = mark;
+  change.removeAttribute('onclick');
   GameRunner.gameBoard[i] = mark;
-  if (!GameRunner.checkWin()){    
+  if (!GameRunner.checkWin()) {
     mark = GameRunner.currentPlayer().getPlayermark();
-    if(mark === "X"){
+    if (mark === 'X') {
       UIController()['player-0'].classList.add('scale');
       UIController()['player-1'].classList.remove('scale');
-    }else {    
+    } else {
       UIController()['player-0'].classList.remove('scale');
       UIController()['player-1'].classList.add('scale');      
     }
   }
-}
-const GameTurn = () => {
-  let turn = 0;
-  let player = GameRunner.playersList.getPlayers()[turn];
-}
+};
 
+const GameRunner = (() => {
+  const gameBoard = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const playersList = Players();
+  let index = 0;
+  const currentPlayer = () => playersList.getPlayers()[index];
+  const changePlayer = () => { index = index === 1 ? 0 : 1; };
+  const createPlayers = () => {
+    const player1 = playersList.add(PlayerFactory(UIController().playerOneInput.value, 'X'));
+    const player2 = playersList.add(PlayerFactory(UIController().playerTwoInput.value, 'O'));
+    UIController().board.classList.remove('hide');
+    const html = `
+      <div class="player player-0"><span>Player 1: </span><span>${player1.getPlayerName()}</span></div>
+      <div class="player player-1"><span>Player 2: </span><span>${player2.getPlayerName()}</span></div>
+    `;
+    UIController().inputDetails.innerHTML = html;
+    UIController()[`player-${index}`].classList.add('scale');
+    UIController().statusBanner.classList.remove('hide');
+    UIController().inputDetails.classList.add('ongoing');
+    UIController().statusText.innerText = `${player1.getPlayerName()}'s turn`;
+  };
+  const reload = () => location.reload();
+  const checkWin = () => CheckWin();
 
-
-
-
-
-document.querySelector("#createP").addEventListener('click', GameRunner.createPlayers)
+  return {
+    // eslint-disable-next-line comma-dangle
+    playersList, createPlayers, currentPlayer, changePlayer, gameBoard, checkWin, reload
+  };
+})();
+document.querySelector('#createP').addEventListener('click', GameRunner.createPlayers)
